b112420f401107c941d23bcb9318d3a646e68369
diixo <anatolii.podoba@gmail.com>
2023-12-17 04:08:15 +0200

    Added json-db, json-client, json-loading implementation into index.html

---
+++ hw_project/quotes/utils/add_news_as_quotes.py
@@ -0,0 +1,26 @@
+import json
+
+class QuotesProvider:
+    def __init__(self) -> None:
+        self.quotes = list()
+
+        # the same as: ./quotes/utils/allainews-news.json
+        with open('quotes/utils/allainews-news.json', 'r', encoding='utf-8') as fd:
+            content = json.load(fd)
+            headings = content.get("headings", dict())
+
+            i = 0
+            for k in headings.keys():
+                i = i+1
+                author = "author-" + str(i)
+                data = dict()
+                data['quote'] = k
+                data['tags'] = ["tag-0", "tag-1"]
+                data['author'] = author
+                self.quotes.append(data)
+
+db_client = QuotesProvider()
+
+
+def get_json_db():
+    return db_client
--- hw_project/quotes/views.py
+++ hw_project/quotes/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
+from .utils.add_news_as_quotes import get_json_db
+
 # Create your views here.
 
 def main(request):
-    return render(request, "quotes/index.html", context={})
+    db = get_json_db()
+    quotes = db.quotes
+    return render(request, "quotes/index.html", context={'quotes': quotes})
