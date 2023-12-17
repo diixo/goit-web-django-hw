aea3c25e216fc19f80313c6f9a7d38b4eabc06a0
diixo <anatolii.podoba@gmail.com>
2023-12-17 16:22:04 +0200

    Pagination implementation

--- hw_project/quotes/views.py
+++ hw_project/quotes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from .utils.add_news_as_quotes import get_json_db
 
 # Create your views here.
 
-def main(request):
+def main(request, page=1):
     db = get_json_db()
     quotes = db.quotes
-    return render(request, "quotes/index.html", context={'quotes': quotes})
+    per_page = 10
+    paginator = Paginator(list(quotes), per_page)
+    quotes_on_page = paginator.page(page)
+    return render(request, "quotes/index.html", context={'quotes': quotes_on_page})
