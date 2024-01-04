6afaab21ee71600b7a8fb58772c3b1859baea53a
diixo <anatolii.podoba@gmail.com>
2024-01-04 02:34:32 +0200

    Added ThreadingExample initial class

--- hw_project/quotes/views.py
+++ hw_project/quotes/views.py
@@ -3,7 +3,26 @@ from django.core.paginator import Paginator
 
 from .utils.add_news_as_quotes import get_json_db
 
-# Create your views here.
+import threading, time 
+
+class ThreadingExample(object):
+
+    def __init__(self, interval=5):
+
+        self.interval = interval
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True                            # Daemonize thread
+        thread.start()                                  # Start the execution
+
+    def run(self):
+
+        while True:
+            # Do something
+            print('Doing something imporant in the background, interval:', self.interval)
+            time.sleep(self.interval)
+
+# example = ThreadingExample()
+
 
 def main(request, page=1):
     db = get_json_db()
