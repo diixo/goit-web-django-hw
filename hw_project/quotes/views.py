2813ea0c5128de8deb21e83435b3fdccdf01686d
diixo <anatolii.podoba@gmail.com>
2024-01-04 03:01:36 +0200

    Updated TimingThread (extended) + renaming

--- hw_project/quotes/views.py
+++ hw_project/quotes/views.py
@@ -5,24 +5,36 @@ from .utils.add_news_as_quotes import get_json_db
 
 import threading, time 
 
-class ThreadingExample(object):
+class TimingThread(object):
 
     def __init__(self, interval=5):
-
+        #super().__init__()
         self.interval = interval
-        thread = threading.Thread(target=self.run, args=())
-        thread.daemon = True                            # Daemonize thread
-        thread.start()                                  # Start the execution
+        self._stop_event = threading.Event()
+        self.thread = threading.Thread(target=self.run, args=())
+        self.thread.daemon = True
+        self.thread.start()
 
     def run(self):
-
-        while True:
+        while not self._stop_event.is_set():
             # Do something
             print('Doing something imporant in the background, interval:', self.interval)
             time.sleep(self.interval)
 
-# example = ThreadingExample()
+    def stop(self):
+        self._stop_event.set()
+        self.thread.join()
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stop()
+
+# example = TimingThread()
+def stop_thread(sender, **kwargs):
+    pass
+    #example.stop()
 
 def main(request, page=1):
     db = get_json_db()
