a71311f599c6ba0155e34bee29d745259bf4097f
diixo <anatolii.podoba@gmail.com>
2024-01-08 03:41:59 +0200

    generate tuples from keywords

--- hw_project/quotes/utils/add_news_as_quotes.py
+++ hw_project/quotes/utils/add_news_as_quotes.py
@@ -1,8 +1,14 @@
 import json
-from core.qq_grammar import str_to_ngrams
+import core.qq_grammar as qq
 
 class QuotesProvider:
+
     def __init__(self) -> None:
+
+        self.unigrams = set()
+        self.bigrams = set()
+        self.trigrams = set()
+
         self.quotes = list()
         self.data = []
 
@@ -18,12 +24,20 @@ class QuotesProvider:
                 stopwords.update([line.replace('\n', '') for line in fsw.readlines()])
             ##########################################################################
 
+            for string in keywords:
+                string = qq.translate(string)
+                ngrams = qq.str_to_ngrams(string, stopwords)
+                for tokens in ngrams:
+                    self.add_tokens(tokens)
+            ##########################################################################
+            print(f"QuotesProvider::uni={len(self.unigrams)}, bi={len(self.bigrams)}, tri={len(self.trigrams)}")
+
             i = 0
-            for k in headings.keys():
-                i = i+1
+            for heading in headings.keys():
+                i = i + 1
                 author = "author-" + str(i)
                 data = dict()
-                data['quote'] = k
+                data['quote'] = heading
                 data['tags'] = ["tag-0", "tag-1"]
                 data['author'] = author
                 self.quotes.append(data)
@@ -31,10 +45,19 @@ class QuotesProvider:
             self.data = self.quotes[-100:][::-1]
             ##########################################################################
             for item in self.data:
-                ngrams = str_to_ngrams(item['quote'], stopwords=stopwords)
+                ngrams = qq.str_to_ngrams(item['quote'], stopwords=stopwords)
                 ngrams = [" ".join([w for w in grm]) for grm in ngrams]
                 item['tags'] = ngrams
                 #item['tags'] = [grm for grm in ngrams if grm.lower() in keywords]
+
+    def add_tokens(self, tokens: list):
+        ngrams_1 = qq.ngrams(tokens, 1)
+        ngrams_2 = qq.ngrams(tokens, 2)
+        ngrams_3 = qq.ngrams(tokens, 3)
+
+        self.unigrams.update(ngrams_1)  # unique inserting
+        self.bigrams.update(ngrams_2)   # unique inserting
+        self.trigrams.update(ngrams_3)  # unique inserting
 
 db_client = QuotesProvider()
 
